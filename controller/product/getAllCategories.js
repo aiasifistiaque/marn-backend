1c02a9765215ad7a8e1419ee7701045e74109497
asifistiaque <asifistiaque.ai@gmail.com>
2021-07-07 01:42:59 +0600

    asds

--- controller/product/getAllCategories.js
+++ controller/product/getAllCategories.js
@@ -1,9 +1,11 @@
 import Product from '../../models/productModel.js';
 import asyncHandler from 'express-async-handler';
 
+const perPage = 50;
+
 export const getAllCategories = asyncHandler(async (req, res) => {
 	let sort = '-createdAt';
-	const categories = await Product.distinct('category').sort(sort);
+	const categories = await Product.distinct('category');
 
 	if (categories) {
 		res.status(200).json(categories);
@@ -15,12 +17,60 @@ export const getAllCategories = asyncHandler(async (req, res) => {
 
 export const getProductByCategory = asyncHandler(async (req, res) => {
 	let sort = '-createdAt';
-	const perPage = 12;
+
 	const page = req.body.page || 0;
 
 	const categories = await Product.find({
 		$and: [
 			{ category: req.params.id },
+			{
+				status: { $nin: ['hidden', 'archived'] },
+			},
+		],
+	})
+		.sort(sort)
+		.skip(page * perPage)
+		.limit(perPage);
+	if (categories) {
+		res.status(200).json(categories);
+	} else {
+		res.status(500);
+		throw new Error('there was an error');
+	}
+});
+
+export const getProductsBySubCategory = asyncHandler(async (req, res) => {
+	let sort = '-createdAt';
+
+	const page = req.body.page || 0;
+
+	const categories = await Product.find({
+		$and: [
+			{ subCategory: req.params.id },
+			{
+				status: { $nin: ['hidden', 'archived'] },
+			},
+		],
+	})
+		.sort(sort)
+		.skip(page * perPage)
+		.limit(perPage);
+	if (categories) {
+		res.status(200).json(categories);
+	} else {
+		res.status(500);
+		throw new Error('there was an error');
+	}
+});
+
+export const getProductsByTag = asyncHandler(async (req, res) => {
+	let sort = '-createdAt';
+
+	const page = req.body.page || 0;
+
+	const categories = await Product.find({
+		$and: [
+			{ tag: req.params.id },
 			{
 				status: { $nin: ['hidden', 'archived'] },
 			},
@@ -63,6 +113,19 @@ export const getSubCategories = asyncHandler(async (req, res) => {
 	let sort = '-createdAt';
 
 	const categories = await Product.distinct('subCategory');
+
+	if (categories) {
+		res.status(200).json(categories);
+	} else {
+		res.status(500);
+		throw new Error('there was an error');
+	}
+});
+
+export const getTAgs = asyncHandler(async (req, res) => {
+	let sort = '-createdAt';
+
+	const categories = await Product.distinct('tag');
 
 	if (categories) {
 		res.status(200).json(categories);
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -4,13 +4,61 @@ import {
 	getSubCategories,
 	getProductByCategory,
 	getHomeProductByCategory,
+	getProductsBySubCategory,
+	getProductsByTag,
+	getTAgs,
 } from '../controller/product/getAllCategories.js';
 
 const router = express.Router();
 
-router.get('/', getAllCategories);
-router.get('/sub', getSubCategories);
-router.post('/cat/:id', getProductByCategory);
 router.get('/homecat/:id', getHomeProductByCategory);
+
+/**
+ * /api/categories
+ * Get names of sub categories
+ * @returns {Promise} array of all sub categories
+ */
+router.get('/', getAllCategories);
+
+/**
+ * /api/categories
+ * Get names of sub categories
+ * @returns {Promise} array of all sub categories
+ */
+router.get('/sub', getSubCategories);
+
+/**
+ * /api/categories
+ * Get names of sub categories
+ * @returns {Promise} array of all tags
+ */
+router.get('/tag', getTAgs);
+
+/**
+ * /api/categories
+ * Get all products of a category
+ * @param {number} page page number
+ * @param {String} id id of the category
+ * @returns {Promise} array of products (50) per page
+ */
+router.post('/cat/:id', getProductByCategory);
+
+/**
+ * /api/categories
+ * Get all products of a sub category
+ * @param {number} page page number
+ * @param {String} id id of the category
+ * @returns {Promise} array of products (50) per page
+ */
+router.post('/subcategory/:id', getProductsBySubCategory);
+
+/**
+ * /api/categories
+ * Get all products of a sub category
+ * @param {number} page page number
+ * @param {String} id id of the category
+ * @returns {Promise} array of products (50) per page
+ */
+router.post('/tag/:id', getProductsByTag);
 
 export default router;
